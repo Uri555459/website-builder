c40f0607ca3de43d6c5c02c880d75f744fe5df48
Uri <uri555459@gmail.com>
2022-02-14 10:48:09 +0300

    Added full header

--- js/main.js
+++ js/main.js
@@ -21,3 +21,130 @@
 // 	menuButton.classList.toggle('menu-button-active');
 // 	menu.classList.toggle('header-active');
 // })
+
+
+const getElement = (tagName, classNames, attributes) => {
+    const element = document.createElement(tagName)
+
+    if (classNames) {
+        element.classList.add(...classNames)
+    }
+
+    if (attributes) {
+        for (const attribute in attributes) {
+            element[attribute] = attributes[attribute]
+        }
+    }
+
+    return element
+}
+
+const createHeader = (param) => {
+    const header = getElement('header')
+    const container = getElement('div', ['container'])
+    const wrapper = getElement('div', ['header'])
+
+    if (param.header.logo) {
+        const logo = getElement('img', ['logo'], {
+            src: param.header.logo,
+            alt: 'Логотип ' + param.title
+        })
+
+        wrapper.append(logo)
+    }
+
+    if (param.header.menu) {
+        const nav = getElement('nav', ['menu-list'])
+        const allLink = param.header.menu.map(element => {
+            const menuLink = getElement('a', ['menu-link'], {
+                href: element.link,
+                textContent: element.title
+            })
+
+            return menuLink
+        })
+
+        nav.append(...allLink)
+        wrapper.append(nav)
+    }
+
+    if (param.header.social) {
+        const social = getElement('div', ['social'])
+
+
+        const allSocial = param.header.social.map(element => {
+            const socialLink = getElement('a', ['social-link'], {
+                href: element.link
+            })
+            const socialImage = getElement('img', [], {
+                src: element.image,
+                alt: element.title
+            })
+
+            socialLink.append(socialImage)
+
+            return socialLink
+        });
+
+        social.append(...allSocial)
+        wrapper.append(social)
+    }
+
+    const menuBtn = getElement('button', ['menu-button'])
+
+
+    header.append(container)
+    container.append(wrapper)
+    container.append(menuBtn)
+
+    return header
+}
+
+const movieConstructor = (selector, options) => {
+    const app = document.querySelector(selector)
+    app.classList.add('body-app')
+    document.title = options.title
+
+    if (options.header) {
+        app.append(createHeader(options))
+    }
+
+}
+
+movieConstructor('.app', {
+    title: 'Ведьмак',
+    header: {
+        logo: './witcher/logo.png',
+        social: [
+            {
+                title: 'twitter',
+                link: 'https://twitter.com',
+                image: './witcher/social/twitter.svg'
+            },
+            {
+                title: 'instagram',
+                link: 'https://instagram.com',
+                image: './witcher/social/instagram.svg'
+            },
+            {
+                title: 'facebook',
+                link: 'https://facebook.com',
+                image: './witcher/social/facebook.svg'
+            }
+        ],
+        menu: [
+            {
+                title: 'Описание',
+                link: '#'
+            },
+            {
+                title: 'Трейлер',
+                link: '#'
+            },
+            {
+                title: 'Отзывы',
+                link: '#'
+            }
+        ]
+    }
+})
